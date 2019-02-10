ee0b2b18961533872d63e64efc4ff8f4a6d0464c
Ali-E-Gokce <38869714+Ali-E-Gokce@users.noreply.github.com>
2019-02-10 16:02:02 -0500

    Update _pdf_to_sentences.py
    Added extra catch errors, more comments

--- _pdf_to_sentences.py
+++ _pdf_to_sentences.py
@@ -3,34 +3,41 @@ from nltk import sent_tokenize
 
 
 def _pdf_to_sentences(pdf_name): #converts the pdf document into sentences
-    # creating a pdf file object 
-    pdfFileObj = open(str(pdf_name), 'rb') 
-      
-    # creating a pdf reader object 
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj) 
+    # creating a pdf file object
+    pdfFileObj = open(str(pdf_name), 'rb')
 
-    # creating a page object 
-    pageObj = pdfReader.getPage(0)
-      
+
+
+    # creating a pdf reader object
+    pdf_reader = PyPDF2.PdfFileReader(pdfFileObj)
+    if pdf_reader.isEncrypted: #decrypts pdf if it can
+        try:
+            pdf_reader.decrypt('')
+        #somtimes it can't, these are the two most common errors I received
+        except (NotImplementedError, PyPDF2.utils.PdfReadError) as e:
+            return
+    # creating a page object
+    pageObj = pdf_reader.getPage(0)
+
     #extracting text from page, returns string
-    text= (pageObj.extractText())
+    try:
+        text = pageObj.extractText()
+    except TypeError:
+        return
+
 
     #Removes new lines on mac OS, Windows and UNIX. NLTK does not do that on it's own.
     text = text.replace('\n', ' ').replace('\r', '')
 
     #dividing the text into sentences. Returns list.
-    
-    sent_text=sent_tokenize(text) 
+
+    sent_text=sent_tokenize(text)
     #creates list of sentences
-    
+
     #the following block will print the first four sentences of a text as a summary. If there are fewer than four sentences in the document, it will just print all sentences.
-    if len(sent_text)>4: 
+    if len(sent_text)>4:
         for sentence in range(0,4):
             print(sent_text[sentence])
     else:
-        for sentence in range(0,len(sentence)):
+        for sentence in range(0,len(sent_text)):
             print(sent_text[sentence])
-                        
-
-    
- 
