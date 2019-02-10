7a9c29841e2f9fac85161572461d0eaa713b2e73
Ali-E-Gokce <38869714+Ali-E-Gokce@users.noreply.github.com>
2019-02-10 16:03:10 -0500

    Create remove.py
    Goes through directories in a top-down fashion, and prints information about the pdf files it finds.

---
+++ remove.py
@@ -0,0 +1,25 @@
+import os
+
+from _pdf_to_sentences import _pdf_to_sentences
+
+from _pdf_info import _pdf_info
+
+
+#looks through directories for pdf files
+for root, dirs, files in os.walk(".", topdown=False):
+   for name in files:
+       if name.endswith(".pdf")
+#some pdf documents can not be opened, and give different exceptions
+#You can't know what kind of erros they might throw.
+           try:
+               #prints psdf info of doc
+               print(_pdf_info(os.path.join(root, name)))
+
+           except:
+               pass
+           try:
+               #prints first 4 sentences of pdf
+               _pdf_to_sentences(os.path.join(root, name))
+           except:
+               pass
+           print("\n")
