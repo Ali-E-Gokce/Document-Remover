ae9860f3b0b04fa634fb04ed6577a94a7a683c97
Ali-E-Gokce <38869714+Ali-E-Gokce@users.noreply.github.com>
2019-02-13 15:36:54 -0500

    Update remove.py

--- remove.py
+++ remove.py
@@ -8,7 +8,7 @@ from _pdf_info import _pdf_info
 #looks through directories for pdf files
 for root, dirs, files in os.walk(".", topdown=False):
    for name in files:
-       if name.endswith(".pdf")
+       if name.endswith(".pdf"):
 #some pdf documents can not be opened, and give different exceptions
 #You can't know what kind of erros they might throw.
            try:
