2521805b0f120d0dd24c7881621e649808220976
Ali-E-Gokce <38869714+Ali-E-Gokce@users.noreply.github.com>
2019-05-08 17:05:37 -0400

    Fixed bug, added comments

--- remove.py
+++ remove.py
@@ -7,26 +7,32 @@ from _pdf_info import _pdf_info
 
 #looks through directories for pdf files
 #add topdown=False as a variable to traverse bottom up
-# "./" is the directory the script is in. You can write another directory path
-for root, dirs, files in os.walk("./"):
+# "/" is the root directiory You can replcae it with another starting directory
+#/Users/username might be a good place to clean
+for root, dirs, files in os.walk("/Users/alierengokcelioglu"):
+
    for name in files:
+       location = root + name
 
        if name.endswith(".pdf"):
-#some pdf documents can not be opened, and give different exceptions
-#You can't know what kind of erros they might throw.
+           location = os.path.join(root, name)
+           #some pdf documents can not be opened, and give different exceptions
+           #You can't know what kind of erros they might throw.
+           print(location)
            try:
                #prints psdf info of doc
-               print(_pdf_info(os.path.join(root, name)))
+               print(_pdf_info(location))
 
            except:
-               pass
+               print("couldn't get pdf info")
+
            try:
                #prints first 4 sentences of pdf
-               _pdf_to_sentences(os.path.join(root, name))
+               _pdf_to_sentences(location)
            except:
-               pass
+               print("couldn't pare pdf into sentences")
            #asks if doc should be removed, user presses y or n
            ans=input("Do you wish to remove this document? y/n. \n") #
            #removes doc if answer is y
            if ans=='y':
-              os.remove(name)
+              os.remove(location)
